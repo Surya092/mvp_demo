c78a2f6b244d52ae8c9f9a8b49931a2f7a91dd41
suryasahu <suryanarayan.sahu@nw18.com>
2024-02-13 16:02:08 +0530

    chore: Refactor

--- mvpdemoTests/HomeViewControllerTest.swift
+++ mvpdemoTests/HomeViewControllerTest.swift
@@ -12,43 +12,42 @@ final class HomeViewControllerTest: XCTestCase {
     
     var storyboard: UIStoryboard!
     var model: HomePageModel!
+    var sut: HomeViewController!
     
     override func setUp() {
         super.setUp()
         storyboard = UIStoryboard(name: "Main", bundle: nil)
         model = HomePageModel(pageItems: ["Red", "Green", "Blue"], title: "Home Page")
+        sut = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as? HomeViewController
+        
     }
     
     func test_navigationTitleSetup() {
-        let homeController = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as! HomeViewController
-        homeController.presenter.model = model
-        let _ = homeController.view
-        homeController.viewDidLoad()
-        XCTAssertEqual(homeController.navigationController?.navigationBar.topItem?.title, model.pageTitle)
+        sut.presenter.model = model
+        let _ = sut.view
+        RunLoop.current.run(until: Date())
+        XCTAssertEqual(sut.navigationController?.navigationBar.topItem?.title, model.pageTitle)
     }
 
     func test_topCellSetup() {
-        let homeController = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as! HomeViewController
-        homeController.presenter.model = model
-        let _ = homeController.view
-        homeController.viewDidLoad()
-        XCTAssertEqual(homeController.tableView(homeController.homeTableView, cellForRowAt: IndexPath(row: 0, section: 0)).textLabel?.text, model.items[0])
+        sut.presenter.model = model
+        let _ = sut.view
+        RunLoop.current.run(until: Date())
+        XCTAssertEqual(sut.tableView(sut.homeTableView, cellForRowAt: IndexPath(row: 0, section: 0)).textLabel?.text, model.items[0])
     }
     
     func test_MiddleCellSetup() {
-        let homeController = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as! HomeViewController
-        homeController.presenter.model = model
-        let _ = homeController.view
-        homeController.viewDidLoad()
-        XCTAssertEqual(homeController.tableView(homeController.homeTableView, cellForRowAt: IndexPath(row: 1, section: 0)).textLabel?.text, model.items[1])
+        sut.presenter.model = model
+        let _ = sut.view
+        RunLoop.current.run(until: Date())
+        XCTAssertEqual(sut.tableView(sut.homeTableView, cellForRowAt: IndexPath(row: 1, section: 0)).textLabel?.text, model.items[1])
     }
     
     func test_BottomCellSetup() {
-        let homeController = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as! HomeViewController
-        homeController.presenter.model = model
-        let _ = homeController.view
-        homeController.viewDidLoad()
-        XCTAssertEqual(homeController.tableView(homeController.homeTableView, cellForRowAt: IndexPath(row: 2, section: 0)).textLabel?.text, model.items[2])
+        sut.presenter.model = model
+        let _ = sut.view
+        RunLoop.current.run(until: Date())
+        XCTAssertEqual(sut.tableView(sut.homeTableView, cellForRowAt: IndexPath(row: 2, section: 0)).textLabel?.text, model.items[2])
     }
 
 }
--- mvpdemoTests/LandingPageViewControllerTest.swift
+++ mvpdemoTests/LandingPageViewControllerTest.swift
@@ -12,49 +12,45 @@ final class LandingPageViewControllerTest: XCTestCase {
 
     var storyboard: UIStoryboard!
     var model: HomePageModel!
+    var sut: HomeViewController!
     
     override func setUp() {
         super.setUp()
         storyboard = UIStoryboard(name: "Main", bundle: nil)
         model = HomePageModel(pageItems: ["Red", "Green", "Blue"], title: "Home Page")
+        sut = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as? HomeViewController
     }
     
     func test_LandingPageNavigation() {
-        let homeController = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as! HomeViewController
-        homeController.presenter.model = model
-        let _ = homeController.view
-        homeController.viewDidLoad()
-        homeController.tableView(homeController.homeTableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+        sut.presenter.model = model
+        let _ = sut.view
+        sut.tableView(sut.homeTableView, didSelectRowAt: IndexPath(row: 0, section: 0))
         RunLoop.current.run(until: Date())
-        guard let _ = homeController.navigationController?.topViewController as? LandingPageViewController else {
+        guard let _ = sut.navigationController?.topViewController as? LandingPageViewController else {
             XCTFail()
             return
         }
     }
     
     func test_LabelSetup_MatchIndex() {
-        let homeController = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as! HomeViewController
-        homeController.presenter.model = model
-        let _ = homeController.view
-        homeController.viewDidLoad()
-        homeController.tableView(homeController.homeTableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+        sut.presenter.model = model
+        let _ = sut.view
+        sut.tableView(sut.homeTableView, didSelectRowAt: IndexPath(row: 0, section: 0))
         RunLoop.current.run(until: Date())
-        let landingPageController = homeController.navigationController?.viewControllers[1] as! LandingPageViewController
+        let landingPageController = sut.navigationController?.viewControllers[1] as! LandingPageViewController
         let _ = landingPageController.view
-        landingPageController.viewDidLoad()
+        RunLoop.current.run(until: Date())
         XCTAssertEqual(landingPageController.pageLabel.text, model.items[0])
     }
     
     func test_LabelSetup_MismatchIndex() {
-        let homeController = (storyboard.instantiateInitialViewController() as! UINavigationController).viewControllers.first as! HomeViewController
-        homeController.presenter.model = model
-        let _ = homeController.view
-        homeController.viewDidLoad()
-        homeController.tableView(homeController.homeTableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+        sut.presenter.model = model
+        let _ = sut.view
+        sut.tableView(sut.homeTableView, didSelectRowAt: IndexPath(row: 0, section: 0))
         RunLoop.current.run(until: Date())
-        let landingPageController = homeController.navigationController?.viewControllers[1] as! LandingPageViewController
+        let landingPageController = sut.navigationController?.viewControllers[1] as! LandingPageViewController
         let _ = landingPageController.view
-        landingPageController.viewDidLoad()
+        RunLoop.current.run(until: Date())
         XCTAssertNotEqual(landingPageController.pageLabel.text, model.items[1])
     }
     
